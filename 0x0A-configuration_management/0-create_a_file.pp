326350d136513cf2b4b4d827e063f0ad898c518a
Ephrem Gebretsion <ephhrem@gmail.com>
2024-03-23 00:05:50 +0300

    puppet file create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
+# creates a file school with content
 file { '/tmp/school':
 ensure  => present,
-owner   => 'www-data'
-group   => 'www-data'
-mode    => '0744'
+owner   => 'www-data',
+group   => 'www-data',
+mode    => '0744',
 content => 'I love Puppet',
 }
