e0f6b4bf86aecfacb22b0f4e82c895e951f78e9d
Ephrem Gebretsion <ephhrem@gmail.com>
2024-03-25 12:50:30 +0300

    configure ssh passwd and instant file using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -8,6 +8,6 @@ match  => 'PasswordAuthentication yes',
 file_line { 'Declare identity file':
 ensure => present,
 path   => '/etc/ssh/ssh_config',
-line   => 'IdentityFile ~/.ssh/yea',
+line   => 'IdentityFile ~/.ssh/school',
 after  => 'Host \*',
 }
