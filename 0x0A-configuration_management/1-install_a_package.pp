4a882a7bf27306f4ad3984e56ceb919421cb3f17
Ephrem Gebretsion <ephhrem@gmail.com>
2024-03-25 23:31:14 +0300

    installing flask 2.1.0 with dependency

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,4 +3,10 @@ package { 'Flask':
 ensure   => '2.1.0',
 name     => 'flask',
 provider => 'pip3',
+require  => Package['Werkzeg'],
+}
+package { 'Werkzeg':
+ensure   => '2.1.1',
+name     => 'werkzeug',
+provider => 'pip3',
 }
