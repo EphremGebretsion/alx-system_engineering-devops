c41898f0013e08d1712cbe025b64c56f397dd6bd
Ephrem Gebretsion <ephhrem@gmail.com>
2024-03-27 03:56:56 +0300

    configuring all with puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,22 +7,20 @@ provider => 'apt',
 file { 'tes':
 ensure  => present,
 path    => '/tmp/tes',
-content => 'Hello World!',
+content => "Hello World!\n",
 }
-file_line { 'root':
-ensure => present,
-path   => '/etc/nginx/sites-available/default',
-line   => 'root /tmp;',
-match  => 'root .*;',
+exec { 'root':
+command => "/usr/bin/sed -i 's-root .*;-root /tmp;-' /etc/nginx/sites-available/default",
 }
 exec { 'sites-enabled':
 command => '/usr/bin/ln -f -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default',
 }
 exec { 'index':
-command => '/usr/bin/sed -i 's/index /index tes /' /etc/nginx/sites-available/default',
+command => "/usr/bin/sed -i 's/index /index tes /' /etc/nginx/sites-available/default",
 }
 exec { 'redirect':
-command => ''/usr/bin/sed -i 's|server_name _;|server_name _;\n\nlocation /redirect_me \
-{\n\t\treturn 301 https:\/\/www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}\
-|' /etc/nginx/sites-available/default'',
+command => "/usr/bin/sed -i 's|server_name _;|server_name _;\\n\\n\\tlocation /redirect_me {\\n\\t\\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\\n\\t}|' /etc/nginx/sites-available/default",
+}
+exec { 'apply':
+command => '/usr/sbin/service nginx restart',
 }
