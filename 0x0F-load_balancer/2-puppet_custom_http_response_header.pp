c5d57b0da69c0b738dec2793403ad90c84e7687b
Ephrem Gebretsion <ephhrem@gmail.com>
2024-04-02 04:37:06 +0300

    configuring header using puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,32 @@
+# this is a puppet script that configures nginx
+package { 'Nginx':
+ensure   => installed,
+name     => 'nginx',
+provider => 'apt',
+}
+file { 'tes':
+ensure  => present,
+path    => '/tmp/tes',
+content => "Hello World!\n",
+}
+exec { 'root':
+command => "/usr/bin/sed -i 's-root .*;-root /tmp;-' /etc/nginx/sites-available/default",
+}
+exec { 'sites-enabled':
+command => '/usr/bin/ln -f -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default',
+}
+exec { 'index':
+command => "/usr/bin/sed -i 's/index /index tes /' /etc/nginx/sites-available/default",
+}
+exec { 'redirect':
+command => "/usr/bin/sed -i 's|server_name _;|server_name _;\\n\\n\\tlocation /redirect_me {\\n\\t\\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\\n\\t}|' /etc/nginx/sites-available/default",
+}
+file_line {'add header':
+ensure => present,
+path   => '/etc/nginx/sites-available/default',
+line   => "\tadd_header X-Served-By ${hostname};",
+after  => '^server {',
+}
+exec { 'apply':
+command => '/usr/sbin/service nginx restart',
+}
